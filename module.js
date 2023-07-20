92c94fd4354633583a5d25e98910564c4f31da68
Carter1190 <65324266+Carter1190@users.noreply.github.com>
2023-07-20 15:08:20 -0230

    Update module.js

--- module.js
+++ module.js
@@ -1,11 +1,10 @@
 import { BarItem, SettingsCategory, SettingsInput } from 'https://flow-os.liquid.is-a.dev/scripts/classes.js';
 
-const customText = new BarItem('javascript');
-new SettingsCategory('custom_text', 'JavascriptEvaluator',
-    new SettingsInput('text', 'Javascript Evaluator', 'alert(1)', 'alert(1)'),
+const customText = new BarItem('custom_text');
+new SettingsCategory('custom_text', 'Custom Text',
+    new SettingsInput('text', 'Text', 'Hello World!', 'Hello World!'),
 );
 
-var button = document.createElement('button');
-button.setAttribute('onclick','eval((config.settings.get("custom_text").text))');
+customText.setText(config.settings.get('custom_text').text);
 
 export default customText;
