90beef9003239c332c5b5888b922be878cab34d7
Carter1190 <65324266+Carter1190@users.noreply.github.com>
2023-07-20 19:58:30 -0230

    Update module.js

--- module.js
+++ module.js
@@ -3,7 +3,7 @@ import config from 'https://flow-os.liquid.is-a.dev/scripts/configManager.js';
 
 const customText = new BarItem('javascript');
 new SettingsCategory('custom_text', 'JavascriptEvaluator',
-    new SettingsTextarea('text', 'Javascript Evaluator', 'alert(1)', 'alert(1)'),
+    new SettingsTextarea('text', 'Javascript Evaluator', 'alert(1)\n', 'alert(1)\n'),
 );
 
 eval(JSON.parse(localStorage.getItem("custom_text")).text)
